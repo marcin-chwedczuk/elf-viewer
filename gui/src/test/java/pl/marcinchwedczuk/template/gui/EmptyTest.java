c57ed82d7f911c585b1a68b332c05000fa609a15
Marcin Chwedczuk <chwedczukm@yahoo.com>
2021-11-09 08:45:53 +0100

    Initial commit

---
+++ gui/src/test/java/pl/marcinchwedczuk/template/gui/EmptyTest.java
@@ -0,0 +1,13 @@
+package pl.marcinchwedczuk.template.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EmptyTest {
+    @Test
+    public void empty() {
+        assertThat(1)
+                .isEqualTo(1);
+    }
+}
