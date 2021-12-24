2490589a633c85135cbe12cbc36e2c80e48ae0cb
Marcin Chwedczuk <0xmarcin+dev@gmail.com>
2021-12-24 13:13:39 +0100

    Show .gnu.build.attributes values in GUI

--- elf-reader/src/main/java/pl/marcinchwedczuk/elfviewer/elfreader/elf/shared/notes/ElfNoteGnuBuildAttribute.java
+++ elf-reader/src/main/java/pl/marcinchwedczuk/elfviewer/elfreader/elf/shared/notes/ElfNoteGnuBuildAttribute.java
@@ -2,7 +2,7 @@ package pl.marcinchwedczuk.elfviewer.elfreader.elf.shared.notes;
 
 import java.util.Objects;
 
-import static pl.marcinchwedczuk.elfviewer.elfreader.elf.shared.notes.ElfGnuBuildAttribute.*;
+import static pl.marcinchwedczuk.elfviewer.elfreader.elf.shared.notes.ElfGnuBuildAttributeType.*;
 
 public class ElfNoteGnuBuildAttribute extends ElfNoteGnu {
     private final byte[] nameBytes;
@@ -26,7 +26,7 @@ public class ElfNoteGnuBuildAttribute extends ElfNoteGnu {
         if (nameBytes.length < 4)
             return null;
 
-        ElfGnuBuildAttribute attributeType = ElfGnuBuildAttribute.fromValue(nameBytes[3]);
+        ElfGnuBuildAttributeType attributeType = ElfGnuBuildAttributeType.fromValue(nameBytes[3]);
         if (attributeType.is(SPECIFICATION_VERSION)) {
             return "version";
         } else if (attributeType.is(STACK_PROTECTOR)) {
@@ -64,10 +64,74 @@ public class ElfNoteGnuBuildAttribute extends ElfNoteGnu {
     }
 
     /**
-     * @return This can return either NATIVE_WORD, String or Boolean
+     * @return This can return either Long, String or Boolean
      * depending on the attribute.
      */
     public Object buildAttributeValue() {
-        return null;
+        // Name format GA<attr-type><attr-name><attr-value>
+        if (nameBytes.length < 5)
+            return null;
+
+        ElfGnuBuildAttributeType attributeType = ElfGnuBuildAttributeType.fromValue(nameBytes[3]);
+
+        int valueStart = -1;
+        if (attributeType.isKnownValue()) {
+            valueStart = 4;
+        } else {
+            // Value starts after name, remember to skip \x00 byte
+            for (int curr = 3; curr < nameBytes.length; curr++) {
+                valueStart = curr + 1;
+                if (nameBytes[curr] == 0) break;
+            }
+        }
+
+        switch ((char)nameBytes[2]) {
+            case '+': return Boolean.TRUE;
+            case '!': return Boolean.FALSE;
+
+            case '$': {
+                StringBuilder value = new StringBuilder();
+                for (int curr = valueStart; curr < nameBytes.length; curr++) {
+                    value.append((char)nameBytes[curr]);
+                }
+                return value.toString();
+            }
+
+            case '*': {
+                // Numeric value in little endian
+                long value = 0;
+                for (int curr = valueStart, shift = 0; curr < nameBytes.length; curr++, shift += 8) {
+                    value |= ((nameBytes[curr] & 0xffL) << shift);
+                }
+
+                // Special cases taken from:
+                // https://github.com/bminor/binutils-gdb/blob/master/binutils/readelf.c#L21049
+                if (attributeType.is(POSITION_INDEPENDENCE)) {
+                    switch ((int)value) {
+                        case 0: return "static";
+                        case 1: return "pic";
+                        case 2: return "PIC";
+                        case 3: return "pie";
+                        case 4: return "PIE";
+                    }
+                } else if (attributeType.is(STACK_PROTECTOR)) {
+                    switch ((int)value) {
+                        /* Based upon the SPCT_FLAG_xxx enum values in gcc/cfgexpand.c.  */
+                        case 0: return "off";
+                        case 1: return "on";
+                        case 2: return "all";
+                        case 3: return "strong";
+                        case 4: return "explicit";
+                    }
+                }
+
+                return (Long)value;
+            }
+
+            default:
+                // Unknown value type
+                return null;
+        }
+
     }
 }
